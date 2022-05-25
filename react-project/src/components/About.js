ab6eb6403f5a093a7ce74cf5ef6716efc766db6c
Nikita20022-cod <grant20022@gmail.com>
2022-05-25 14:19:45 +0500

    version one

---
+++ react-project/src/components/About.js
@@ -0,0 +1,27 @@
+function About() {
+    return (
+      <section className="about">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-3">
+                    <h2>
+                        О нас
+                    </h2>
+                </div>
+                <div className="col-lg-7">
+                    <p className="about-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="col-12">
+                    <p className="about-bot text-center">
+                      Мы всегда рядом для помощи
+                    </p>
+                </div>
+            </div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default About;
